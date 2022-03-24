a012f72252bcb39fd0fa7b33c0b349e17f1d5e16
Vallraven <vallraven@yandex.ru>
2022-03-24 21:01:55 +0300

    переместил обработчик с картинки на пин

--- src/components/Element/Element.js
+++ src/components/Element/Element.js
@@ -1,11 +1,28 @@
 import styles from './Element.module.scss';
 import icon from './icons/pin.png';
+import cn from 'classnames';
 
 export const Element = ({children, pin, ...props}) => {
+
+    const onMouseEnterHandler = (e) => {
+        if (e.target.classList.contains(styles.pin)) {
+            e.target.parentElement.classList.add(styles.hover);
+        }
+    }
+
+    const onMouseLeaveHandler = (e) => {
+        if (e.target.classList.contains(styles.pin)) {
+            e.target.parentElement.classList.remove(styles.hover);
+        }
+    };
+
     return (
         <div className={styles.element} {...props}>
             {children}
-            <img src={icon} style={pin} alt="2x2" className={styles.pin}/>
+            <img src={icon} style={pin} alt="2x2" className={styles.pin}
+                 onMouseEnter={onMouseEnterHandler}
+                 onMouseLeave={onMouseLeaveHandler}
+                />
         </div>
     );
 };

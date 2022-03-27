891a152e3d548f4293bc8f6344d00bca082e0a8e
Vallraven <vallraven@yandex.ru>
2022-03-28 00:02:19 +0300

    частично добавил запрос с сервера

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,19 +1,40 @@
 import {useState} from 'react';
+import axios from 'axios';
 import {Element, Modal} from '../../components';
 import styles from './Content.module.scss';
 import {collection} from '../../date';
 
 export const Content = ({...props}) => {
-    const [type, showModal] = useState(false);
+    const domain = 'https://media.2x2tv.ru',
+        key = 'fc62110ca6739b16228cceff36',
+        param = '',
+        api = (link) => `${domain}/ghost/api/v3/content/posts/slug/${link}/?key=${key}`;
+
+    const [type, showModal] = useState(null);
+    const [content, setContent] = useState(null);
+
+    // <div dangerouslySetInnerHTML={{ __html: content }} />
+
+    const modalContent = content ? {content} : <h2>Что-то пошло не так...</h2>;
+
+    const getDate = (link) => {
+        axios.get(api(link))
+            .then(function (response) {
+                const data = response.data.posts[0].html;
+                setContent(() => data);
+                showModal('first');
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
 
     const onClickHandle = (e) => {
         const link = e.target.getAttribute('data-link');
-        showModal('first');
+        if (link !== null) getDate(link);
     }
 
-    const closeModal = () => {
-        showModal(false);
-    }
+    const closeModal = () => showModal(false);
 
     const generateElements = ({id, name, style, img, pin, link}) => {
         return (
@@ -22,11 +43,12 @@ export const Content = ({...props}) => {
             </Element>
         )
     }
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.bg}></div>
             {collection && collection.map(item => generateElements(item))}
-            {(type === 'first') ? <Modal visible={true} onClose={closeModal}/> : null}
+            {(type === 'first') ? <Modal visible={true} onClose={closeModal}>{modalContent}</Modal> : null}
         </div>
     );
 };

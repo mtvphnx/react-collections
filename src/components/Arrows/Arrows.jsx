1f7909ea1280cb8df235bb6423a609bb561800e0
Vallraven <vallraven@yandex.ru>
2022-05-04 07:41:55 +0300

    изменил вывод стрелок

--- src/components/Arrows/Arrows.jsx
+++ src/components/Arrows/Arrows.jsx
@@ -2,12 +2,14 @@ import cn from 'classnames';
 import styles from './Arrows.module.scss';
 
 export const Arrows = ({handlerClick}) => {
+    const data = ['top', 'right', 'left', 'bottom'];
+    const elements = data.map(position => {
+        return <div onClick={handlerClick} data-arrow={position} className={cn(styles.arrow, `on-${position}`)}></div>
+    })
+
     return (
         <div className={styles.arrows}>
-            <div onClick={handlerClick} data-arrow="top"  className={cn(styles.arrow, 'on-top')}></div>
-            <div onClick={handlerClick} data-arrow="right" className={cn(styles.arrow, 'on-right')}></div>
-            <div onClick={handlerClick} data-arrow="left" className={cn(styles.arrow, 'on-left')}></div>
-            <div onClick={handlerClick} data-arrow="bottom" className={cn(styles.arrow, 'on-bottom')}></div>
+            {elements}
         </div>
     )
 }

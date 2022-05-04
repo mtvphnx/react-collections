084e09a87567331dfa66e5bfaad86a2edc33b6d6
Vallraven <vallraven@yandex.ru>
2022-05-04 07:08:50 +0300

    исправил ссылку в логотипе

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import icon from '../../resources/icons/logo.svg';
 
 export const Logo = ({className, color}) => {
     return (
-        <a href="/" className={cn(styles.link, className)}>
+        <a href={process.env.PUBLIC_URL} className={cn(styles.link, className)}>
             <img src={icon}
                  className={styles.logo}
                  alt="logo"

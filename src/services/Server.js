6495088d3711c33bb89b26ca0ce4a12c349ccb1d
Vallraven <vallraven@yandex.ru>
2022-10-04 14:04:14 +0300

    увеличил лимит для api

--- src/services/Server.js
+++ src/services/Server.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default class Server {
     _apiBase = 'https://media.2x2tv.ru/ghost/api/v3/content/posts';
     _apiKey = '?key=fc62110ca6739b16228cceff36';
-    _apiFields = '&filter=tag:collections';
+    _apiFields = '&filter=tag:collections&limit=all';
 
     getResource = async (link) => {
         const api = link ? `${this._apiBase}/slug/${link}/${this._apiKey}` : `${this._apiBase}/${this._apiKey}${this._apiFields}`;
